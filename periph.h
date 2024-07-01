0693172f278166d55c3ae0080c157e77bcd4671e
Anaqvi2020 <93833795+Anaqvi2020@users.noreply.github.com>
2024-07-01 13:14:20 -0700

    Update periph.h

--- periph.h
+++ periph.h
@@ -7,27 +7,9 @@
 #define PERIPH_H
 
 
-////////// SONAR UTILITY FUNCTIONS ///////////
-
-void init_sonar(){
-	sei();					/* Enable global interrupt */
-	TIMSK1 = (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
-	TCCR1A = 0;
-}
-
-// read_sonar function implmentation moved to timerISR.h file
-// double read_sonar()
-
-////////// ADC UTILITY FUNCTIONS ///////////
-
 void ADC_init() {
   ADMUX = (1<<REFS0);
 	ADCSRA|= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-	// ADEN: setting this bit enables analog-to-digital conversion.
-	// ADSC: setting this bit starts the first conversion.
-	// ADATE: setting this bit enables auto-triggering. Since we are
-	//        in Free Running Mode, a new conversion will trigger whenever
-	//        the previous conversion completes.
 }
 
 unsigned int ADC_read(unsigned char chnl){
@@ -43,6 +25,4 @@ unsigned int ADC_read(unsigned char chnl){
 	return ((high << 8) | low) ;
 }
 
-////////// ADC AND SONAR UTILITY FUNCTIONS ///////////
-
-#endif /* PERIPH_H */
+#endif 
